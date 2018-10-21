4796129c13011831509ae77d3152c06e9ea15099
FlashDXY <w276455978@163.com>
2018-10-21 20:28:06 +0800

    Add files via upload

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -4,24 +4,30 @@ MAX_NUM = float('inf')
 
 
 class PID(object):
-    def __init__(self, kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM):
+    def __init__(self, kp, ki, kd):
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.min = mn
-        self.max = mx
-
-        self.int_val = self.last_error = 0.
+        self.int_val = self.last_int_val = self.last_error = 0.
 
     def reset(self):
         self.int_val = 0.0
+        self.last_int_val = 0.0
+        self.last_error = 0.
 
-    def step(self, error, sample_time):
+    def freeze(self):
+        self.last_error = 0.
+
+    def step(self, error, sample_time, mn=MIN_NUM, mx=MAX_NUM):
+        self.last_int_val = self.int_val
+        self.min = mn
+        self.max = mx
 
         integral = self.int_val + error * sample_time;
         derivative = (error - self.last_error) / sample_time;
 
-        val = self.kp * error + self.ki * integral + self.kd * derivative;
+        y = self.kp * error + self.ki * self.int_val + self.kd * derivative;
+        val = max(self.min, min(y, self.max))
 
         if val > self.max:
             val = self.max
